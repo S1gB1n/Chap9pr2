701e57b654952c4818f1bc25c917b778476ae129
S1gB1n <58967059+S1gB1n@users.noreply.github.com>
2021-11-10 11:30:50 -0700

    Add files via upload

--- ch09-proj2.js
+++ ch09-proj2.js
@@ -1,6 +1,19 @@
 
 /* add your code here */
+const X = 0;
+const Y = 1;
+const DESC = 2;
+
 const painting_content = JSON.parse(content);
+
+function find_painting(id){
+    for(let i in painting_content){
+        if(painting_content[i].id == id){
+            return painting_content[i]
+        }
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', () =>{
 
@@ -21,27 +34,79 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     //#4
     //event delegation to process all painting list
+    /*  adding event delegation on an element will make anything inside the element becomes clickable
+        but to add event to those stuff inside, we need to add a condition.
+        ex. 
+            const target = e.target;
+            if(target) { ... do stuff ...}
+    */
+    let first_run = true;
     var painting_list = document.getElementById('paintings');
     painting_list.addEventListener('click', function(e){
         const target = e.target;
         if (target){
-            console.log("clicked: " + target.id); // test
+            
+            if(first_run){
+                first_run = false;
+            }else{
+                var figure_element = document.querySelector('figure');
+                var c_box = document.querySelectorAll('.box');
+                console.log(c_box);
+                console.log(figure_element);        
+                c_box.forEach(element => figure_element.removeChild(element));
+            }
 
-            var figure_element = document.getElementsByTagName('figure');
-            figure_element.innerHTML = "";
-            //const painting = painting_content.find(element => element = );
+            const painting = find_painting(target.id);
+            document.querySelector('#full').src = "images/large/" + painting.id + ".jpg";
+            document.querySelector('#title').innerHTML = painting.title;
+            document.querySelector('#artist').innerHTML = painting.artist;     
+            console.log(painting); // test
 
-            console.log(figure_element); // test
+            //#5
+            // add the boxes (div) wtih description
+            var painting_feature = painting.features;
+            var node_features;
+            for(let i in painting_feature){
+                node_features = document.createElement('div');
+                node_features.setAttribute('class', 'box');
+                node_features.style.position = 'absolute';
+                node_features.style.left =  painting_feature[i].upperLeft[X] + 'px';
+                node_features.style.top = painting_feature[i].upperLeft[Y] + 'px';
+                let width = painting_feature[i].lowerRight[X] - painting_feature[i].upperLeft[X];
+                let height = painting_feature[i].lowerRight[Y] - painting_feature[i].upperLeft[Y];
+                node_features.style.width = width + 'px';
+                node_features.style.height = height + 'px';
+
+                //#6 
+                //Mouseover and Mouseout
+                node_features.addEventListener('mouseover', function(event){
+                    const e_target = event.target;
+                    if(e_target){
+                        let desc = painting_feature[i].description;
+                        document.querySelector('#description').innerHTML = desc;                     
+                    }
+                });
+                node_features.addEventListener('mouseout', function(event){
+                    const e_target = event.target;
+                    if(e_target){
+                        document.querySelector('#description').innerHTML = "";                     
+                    }
+                });
+                
+
+                document.querySelector('figure').appendChild(node_features);
+            }
+            
+
+
         }
     });    
 
-    const a = painting_content.find(element => element = '095010'); //this is wrong
-    console.log(a.artist);
+    
 });
 
 /*
     To add even click on the picture use "event delegation" (see in the notes page: 45)
-
     find():
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
  
